e2cb1a491f6dce0c8c0f4a2a69eef00fcfbff971
João <joaovictorca2004@gmail.com>
2022-04-14 18:17:52 -0300

    version 90%

--- main.js
+++ main.js
@@ -1,33 +1,25 @@
-const LinkSocialMedia = {
-  github: 'joaovic-tech',
-  youtube: 'joaovic.tech',
-  facebook: 'joaovic.tech',
-  instagram: 'joaovic.tech',
-  twitter: 'joaovic.tech'
-}
-
-function changeSocialMediaLinks() {
-  for (let li of socialLinks.children) {
-    const social = li.getAttribute('class')
-
-    li.children[0].href = `https://${social}.com/${LinkSocialMedia[social]}`
-  }
-}
-
-changeSocialMediaLinks()
-
-function getGitHubProfileOnfos() {
-  const url = `https://api.github.com/users/${LinkSocialMedia.github}`
-
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
+function githubProfile(githubLogin) {
+  const url = `https://api.github.com/users/${githubLogin}`
+  fetch(url).then(response => response.json()).then(data => {
       userName.textContent = data.name
       userBio.textContent = data.bio
       userLink.href = data.html_url
       userImage.src = data.avatar_url
       userLogin.textContent = data.login
-    })
+  })
 }
+githubProfile('joaovic-tech')
 
-getGitHubProfileOnfos()
+/* Evento de clique para pegar o valor do input e envias para function githubProfile*/
+btnSearch.addEventListener('click', function(e) {
+  e.preventDefault();
+  githubProfile(IUserGithub.value)
+})
+
+IUserGithub.addEventListener('click', function(){
+  if(label.className == 'active'){
+    label.classList.remove('active')
+  }else {
+    label.classList.add('active')
+  }
+})
