32b0a9531c0e559b8714e5587ddd8f119047aa95
João <joaovictorca2004@gmail.com>
2022-04-20 19:30:38 -0300

    darkMode lightMode complete

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 btnSearch.addEventListener('click', function (e) {
   e.preventDefault()
-  if(ISearch.value.length > 0){
-    GithubProfile(ISearch.value)
-  }else{
-    alert('empty text field')
+  if (sidebar.className == 'active') {
+    ISearch.value.length > 0
+      ? GithubProfile(ISearch.value)
+      : alert('Empty text field')
+  } else {
+    sidebar.classList.add('active')
   }
 })
 
@@ -12,13 +14,13 @@ function GithubProfile(githubLogin) {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      if(data.message != 'Not Found'){
+      if (data.message != 'Not Found') {
         userName.textContent = data.name
         userLogin.textContent = data.login
         userBio.textContent = data.bio
         userLink.href = data.html_url
         userImage.src = data.avatar_url
-      }else{
+      } else {
         alert('User Not Found')
       }
     })
@@ -31,37 +33,12 @@ function ShowSidebar() {
 }
 
 // Show Modals Lists
-function ShowModal(m){
+function ShowModal(m) {
   const modalContainer = document.getElementById(m)
-  
   modalContainer.classList.toggle('showModal')
 }
 
-function CloseModal(m){
+function CloseModal(m) {
   const modal = document.getElementById(m)
   modal.classList.remove('showModal')
-}
-
-const root = document.documentElement; 
-function ColorThemes(color){
-  if(color == 'default'){
-    root.style.setProperty('--color-primary', '#FF008E');
-    root.style.setProperty('--color-secundary', '#FFCD1E');
-  }
-  if(color == 'blue'){
-    root.style.setProperty('--color-primary', '#0047FF');
-    root.style.setProperty('--color-secundary', '#57E5DC');
-  }
-  if(color == 'green'){
-    root.style.setProperty('--color-primary', '#00FF19');
-    root.style.setProperty('--color-secundary', '#57E5DC');
-  }
-  if(color == 'purple'){
-    root.style.setProperty('--color-primary', '#D800B9');
-    root.style.setProperty('--color-secundary', '#8257E5');
-  }
-  root.style.setProperty('--img-background', `url(../images/${color}-background.svg)`);
-  root.style.setProperty('--img-light', `url(../images/${color}-light.svg)`);
-  root.style.setProperty('--img-range', `url(../images/${color}-range.svg)`);
-  closeModal.click()
 }
