e461addc93e7f1ded6bb26c26c0a28c26151e853
João <joaovictorca2004@gmail.com>
2022-04-21 17:03:19 -0300

    infoUser && hide nav view nav add

--- js/main.js
+++ js/main.js
@@ -1,32 +1,3 @@
-btnSearch.addEventListener('click', function (e) {
-  e.preventDefault()
-  if (sidebar.className == 'active') {
-    ISearch.value.length > 0
-      ? GithubProfile(ISearch.value)
-      : alert('Empty text field')
-  } else {
-    sidebar.classList.add('active')
-  }
-})
-
-function GithubProfile(githubLogin) {
-  const url = `https://api.github.com/users/${githubLogin}`
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      if (data.message != 'Not Found') {
-        userName.textContent = data.name
-        userLogin.textContent = data.login
-        userBio.textContent = data.bio
-        userLink.href = data.html_url
-        userImage.src = data.avatar_url
-      } else {
-        alert('User Not Found')
-      }
-    })
-}
-GithubProfile('joaovic-tech')
-
 // function show siderbar
 function ShowSidebar() {
   sidebar.classList.toggle('active')
@@ -35,10 +6,18 @@ function ShowSidebar() {
 // Show Modals Lists
 function ShowModal(m) {
   const modalContainer = document.getElementById(m)
-  modalContainer.classList.toggle('showModal')
+  modalContainer.classList.add('showModal')
 }
 
 function CloseModal(m) {
   const modal = document.getElementById(m)
   modal.classList.remove('showModal')
+}
+
+
+// Function hide/view nav
+
+function HideSidebar(){
+  sidebar.classList.toggle('hide')
+  hideSidebar.classList.toggle('icon')
 }
